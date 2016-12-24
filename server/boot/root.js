72082a766ee56d7263e4f36235677ccc2afbd51a
jainisenough <sonu_theflirtyguy20@yahoo.co.in>
2016-12-24 13:06:36 +0530

    Redirect all routes to nowweb.tk

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,6 +2,9 @@
 
 module.exports = function(server) {
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  var domain = server.get('domain');
+  router.get('*', function(req, res) {
+    res.redirect('http' + (domain.port === 443 ? 's':'') + '://' + domain.host);
+  });
   server.use(router);
 };
